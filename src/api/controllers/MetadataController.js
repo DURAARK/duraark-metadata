228c9cc8f27c0e1ac82dfa404d0496d55ee34c39
martinhecher <martin.hecher@fraunhofer.at>
2015-08-05 18:49:35 +0200

    added API doc in version 0.7.0

--- src/api/controllers/MetadataController.js
+++ src/api/controllers/MetadataController.js
@@ -8,7 +8,80 @@
 
 var duraark = require('../../lib/duraark');
 
+/**
+ * @apiDefine MetadataSuccess
+ * @apiSuccess (File) {String} path Location of the File.
+ * @apiSuccess (File) {String} type Type of the File ('e57' or 'ifc-spf').
+ * @apiSuccess (File) {Date} createdAt Creation time of the database instance.
+ * @apiSuccess (File) {Date} updatedAt Last modification time of the database instance.
+ * @apiSuccess (File) {Number} id Database instance's unique ID.
+ * @apiSuccess (File) {Object} metadata The extracted metadata is returned as [JSON-LD](http://json-ld.org/). The data is logically separated into a 'physicalAsset' and a 'digitalObject' section and follows the [buildM+](https://github.com/DURAARK/Schemas/blob/master/rdf/buildm%2Bv3.1.rdf) schema description.
+ */
+
 module.exports = {
+  /**
+ * @api {get} /file/:id Request cached metadata
+ * @apiVersion 0.7.0
+ * @apiName GetMetadata
+ * @apiGroup Metadata
+ * @apiPermission none
+ *
+ * @apiDescription Requests cached metadata from the server.
+ *
+ * @apiParam {Number} id File's unique ID.
+ *
+ * @apiExample {curl} Example usage:
+ * curl -i http://data.duraark.eu/services/api/metadata/file/1
+ *
+ * @apiUse MetadataSuccess
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "path": "/duraark-storage/files/Nygade_Scan1001.e57",
+ *        "type": "e57",
+ *        "createdAt": "2015-08-05T15:20:24.963Z",
+ *        "updatedAt": "2015-08-05T15:20:25.005Z",
+ *        "id": 1,
+ *        "metadata": {
+ *          "physicalAsset": { ... JSON-LD data ... },
+ *          "digitalObject": { ... JSON-LD data ... }
+ *      }
+ *
+ * @apiError NotFound The metadata information was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     Not Found
+ *
+ */
+
+  /**
+   * @api {post} /metadata Extract metadata
+   * @apiVersion 0.7.0
+   * @apiName PostMetadata
+   * @apiGroup Metadata
+   * @apiPermission none
+   *
+   * @apiDescription Extracts metadata from the given File.
+   *
+   * @apiParam (File) {String} path Location of the File as provided by the [DURAARK Sessions API](http://data.duraark.eu/services/api/sessions/).
+   * @apiParam (File) {String} type Type of the File ('e57' or 'ifc-spf').
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *        "path": "/duraark-storage/files/Nygade_Scan1001.e57",
+   *        "type": "e57",
+   *        "createdAt": "2015-08-05T15:20:24.963Z",
+   *        "updatedAt": "2015-08-05T15:20:25.005Z",
+   *        "id": 1,
+   *        "metadata": {
+   *          "physicalAsset": { ... JSON-LD data ... },
+   *          "digitalObject": { ... JSON-LD data ... }
+   *      }
+   *
+   */
   create: function(req, res, next) {
     var metadataExtraction = new duraark.MetadataExtraction();
 
@@ -27,7 +100,6 @@ module.exports = {
     var MetadataExtractorE57 = require('../../lib/tools/E57Extract/app');
     var myTest = new MetadataExtractorE57();
     var output = myTest.json2xml(req.body);
-
 
     res.send(output);
   }
---
+++ src/assets/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "duraark-metadata",
+  "version": "0.7.0",
+  "title": "DURAARK Metadata Service",
+  "url": "http://data.duraark.eu/services/api/metadata",
+  "sampleUrl": "http://data.duraark.eu/services/api/metadata",
+  "description": "Service for metadata extraction from IFC-SPF, E57 and (soon) HDF5 files.",
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-08-05T16:46:31.469Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
